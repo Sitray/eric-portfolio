6dd3ba2d933ce8b0483912560884ca7d5918c5ae
Eric <ericmares13@gmail.com>
2021-11-30 14:05:56 +0100

    landing page visible

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="lp-container">
+      <div> Landing Page</div>
+      <div className="lp-text">
+        <h1>Eric Marès Aguilera</h1>
+        <p>Front-end Developer</p>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
