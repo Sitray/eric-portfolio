700f4394c9b1e8bd046e637474aca8e7535af06f
Eric <ericmares13@gmail.com>
2021-11-30 16:04:24 +0100

    add github icons to the landing page and some styling

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import { GoMarkGithub } from 'react-icons/go';
+
 import './LandingPage.css';
 
 function LandingPage() {
   return (
     <div className="lp-container">
-      <div> Landing Page</div>
       <div className="lp-text">
+        <img src="test.svg" alt="logo" className="lp-logo" />
         <h1>Eric Marès Aguilera</h1>
-        <p>Front-end Developer</p>
+        <span>Front-end Developer</span>
+      </div>
+      <div className="lp-icons">
+        <a href="https://github.com/Sitray" target="_blank" rel="noreferrer">
+          <GoMarkGithub color="black" />
+        </a>
       </div>
     </div>
   );
