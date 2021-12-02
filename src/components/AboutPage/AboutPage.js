8f7f70fed180255672a9130088887984c7044166
Eric <ericmares13@gmail.com>
2021-12-02 16:27:28 +0100

    about me text full responsive

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -4,15 +4,25 @@ import './AboutPage.css';
 
 function AboutPage({ id }) {
   return (
-    <div className="ab-container" id={id}>
-      <div className="ab-title">
-        <h1>About me page!!!!!!!!!</h1>
-      </div>
-      <div className="ab-content">
-        <img src="eric-foto.jfif" alt="Foto" className="lp-foto" />
-        <p className="ab-text">
-          kasjhdljasdlkjadflkjsalfkjslkfdjlskdjflksdfjlksdjfdlksadjflksdjflksajf
-        </p>
+    <div className="ab-section" id={id}>
+      <div className="ab-container">
+        <div className="ab-title">
+          <h1>About me</h1>
+        </div>
+        <div className="ab-content">
+          <img src="eric-foto.jfif" alt="Foto" className="ab-foto" />
+          <article className="ab-text-container">
+            <h3>Profile</h3>
+            <p>
+              Soy un entusiasta del desarrollo web y de los diseños
+              minimalistas. <br />
+              Aamante de las nuevas tecnologías y del cine.
+            </p>
+
+            <h3>Experience</h3>
+            <p>Pasiona 2016-2018</p>
+          </article>
+        </div>
       </div>
     </div>
   );
