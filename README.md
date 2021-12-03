727a179c3a538492fb84b07fa1c84fa48d95640a
Eric <ericmares13@gmail.com>
2021-12-03 12:42:10 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# WORK IN PROGRESS 
+# WORK IN PROGRESS
+
+For the icons i'm using https://devicon.dev/ thanks to them :)
--- src/components/Logos/Logos.js
+++ src/components/Logos/Logos.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-// for default all icons will be black and 30px
+// for default all icons will be black and the font-size 30px.
+
+// This component will work with https://devicon.dev/ thx to devicon.:)
 
 function Logos({ iconName, fontSizeIcon = '30px', colorIcon = 'black' }) {
   const styles = {
@@ -10,7 +12,7 @@ function Logos({ iconName, fontSizeIcon = '30px', colorIcon = 'black' }) {
 
   return (
     <div style={styles}>
-      <i className={`${iconName}`} />
+      <i className={iconName} />
     </div>
   );
 }
